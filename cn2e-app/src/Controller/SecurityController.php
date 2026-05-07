9fe0dc1ce85b680feb881851889d4cc1f1efdc37
Maëlle Nioche <maelle.nioche@gmail.com>
2026-05-07 21:19:24 +0200

    feat(auth): add login form and authentication flow

---
+++ cn2e-app/src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/connexion', name: 'app_login')]
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response 
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/auth.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'activeForm' => 'login',
+        ]);
+    }
+
+    #[Route(path: '/deconnexion', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('Cette méthode peut être vide - elle sera interceptée par la clé de sécurité de déconnexion dans votre configuration.');
+    }
+}
