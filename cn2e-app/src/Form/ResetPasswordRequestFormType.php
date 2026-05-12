e6d96ec1fc43a3b5a8e85b403cfd83c60f05ccea
Maëlle Nioche <maelle.nioche@gmail.com>
2026-05-12 17:47:55 +0200

    feat(security): integrate FormSecuritySubscriber into password reset request form

--- cn2e-app/src/Form/ResetPasswordRequestFormType.php
+++ cn2e-app/src/Form/ResetPasswordRequestFormType.php
@@ -2,16 +2,24 @@
 
 namespace App\Form;
 
+use App\EventSubscriber\FormSecuritySubscriber;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ResetPasswordRequestFormType extends AbstractType
 {
+    public function __construct(
+        private FormSecuritySubscriber $subscriber
+    ) {}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder->addEventSubscriber($this->subscriber);
+
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'form.reset_password_request.email',
@@ -22,6 +30,15 @@ class ResetPasswordRequestFormType extends AbstractType
                     ),
                 ],
             ])
+            // Champ caché anti-spam
+            ->add('website', HiddenType::class, [
+                'required' => false,
+                'mapped' => false,
+                'attr' => [
+                    'autocomplete' => 'off',
+                    'tabindex' => '-1',
+                ],
+            ]);
         ;
     }
 
