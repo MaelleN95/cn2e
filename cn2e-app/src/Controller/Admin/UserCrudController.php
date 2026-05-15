6e4c6e566fb66e63cf5fc836dd1f3bd77e4af48f
Maëlle Nioche <maelle.nioche@gmail.com>
2026-05-15 11:51:29 +0200

    clean(admin): remove unused namespaces in UserCrud

--- cn2e-app/src/Controller/Admin/UserCrudController.php
+++ cn2e-app/src/Controller/Admin/UserCrudController.php
@@ -11,10 +11,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
