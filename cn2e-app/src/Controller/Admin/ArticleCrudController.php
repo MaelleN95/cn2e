f55d5ce8b483567d4021aabfc882489ed8e0ff32
Maëlle Nioche <maelle.nioche@gmail.com>
2026-05-15 11:50:36 +0200

    feat(article): set author automatically from authenticated user on creation

--- cn2e-app/src/Controller/Admin/ArticleCrudController.php
+++ cn2e-app/src/Controller/Admin/ArticleCrudController.php
@@ -3,18 +3,25 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class ArticleCrudController extends AbstractCrudController
 {
+
+    public function __construct(
+        private Security $security,
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Article::class;
@@ -52,6 +59,20 @@ class ArticleCrudController extends AbstractCrudController
         yield BooleanField::new('isMembersOnly', 'admin.article.isMembersOnly');
 
         yield AssociationField::new('author', 'admin.article.author')
-            ->autocomplete();
+            ->hideOnForm();
+    }
+
+    // Définir l'auteur lors de la création d'une actualité
+    public function persistEntity(
+        EntityManagerInterface $entityManager,
+        $entityInstance
+    ): void {
+        if (!$entityInstance instanceof Article) {
+            return;
+        }
+
+        $entityInstance->setAuthor($this->security->getUser());
+
+        parent::persistEntity($entityManager, $entityInstance);
     }
 }
