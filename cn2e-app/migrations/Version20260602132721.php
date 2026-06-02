7f204a1ee62ce72c9eee573eda3c7b838be6615c
Maëlle Nioche <maelle.nioche@gmail.com>
2026-06-02 15:29:30 +0200

    fix(migration): set updated_at to nullable true

--- cn2e-app/migrations/Version20260602132721.php
+++ cn2e-app/migrations/Version20260602132721.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260601222851 extends AbstractMigration
+final class Version20260602132721 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,13 +19,11 @@ final class Version20260601222851 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD updated_at DATETIME NOT NULL, CHANGE profile_picture image_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD updated_at DATETIME DEFAULT NULL, CHANGE profile_picture image_name VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP updated_at, CHANGE image_name profile_picture VARCHAR(255) DEFAULT NULL');
     }
 }
